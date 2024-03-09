b6f909fe0a2d11ee7f48b98b39c1fc4b95e6e29f
Enz0Z <enz0z@users.noreply.github.com>
2024-03-09 12:01:44 +0000

    refactor: updated to "latest" artifact

--- artifact/windows/citizen/scripting/lua/natives_server.lua
+++ artifact/windows/citizen/scripting/lua/natives_server.lua
@@ -2747,18 +2747,27 @@ function Global.SetVehicleDoorBroken(vehicle, doorIndex, deleteDoor)
 	return _in(0x8147fea7, vehicle, doorIndex, deleteDoor)
 end
 
---- // Source GTA VC miss2 leak, matching constants for 0/2/4, testing
--- // They use 10 in am_mp_property_int, don't know what it does atm.
--- enum eCarLock {
--- CARLOCK_NONE = 0,
--- CARLOCK_UNLOCKED = 1,
--- CARLOCK_LOCKED = 2,
--- CARLOCK_LOCKOUT_PLAYER_ONLY = 3,
--- CARLOCK_LOCKED_PLAYER_INSIDE = 4,
--- CARLOCK_LOCKED_INITIALLY = 5,
--- CARLOCK_FORCE_SHUT_DOORS = 6,
--- CARLOCK_LOCKED_BUT_CAN_BE_DAMAGED = 7
+--- Locks the doors of a specified vehicle to a defined lock state, affecting how players and NPCs can interact with the vehicle.
+-- ```
+-- NativeDB Introduced: v323
+-- ```
+-- ```cpp
+-- enum eVehicleLockState {
+-- VEHICLELOCK_NONE = 0, // No specific lock state, vehicle behaves according to the game's default settings.
+-- VEHICLELOCK_UNLOCKED = 1, // Vehicle is fully unlocked, allowing free entry by players and NPCs.
+-- VEHICLELOCK_LOCKED = 2, // Vehicle is locked, preventing entry by players and NPCs.
+-- VEHICLELOCK_LOCKOUT_PLAYER_ONLY = 3, // Vehicle locks out only players, allowing NPCs to enter.
+-- VEHICLELOCK_LOCKED_PLAYER_INSIDE = 4, // Vehicle is locked once a player enters, preventing others from entering.
+-- VEHICLELOCK_LOCKED_INITIALLY = 5, // Vehicle starts in a locked state, but may be unlocked through game events.
+-- VEHICLELOCK_FORCE_SHUT_DOORS = 6, // Forces the vehicle's doors to shut and lock.
+-- VEHICLELOCK_LOCKED_BUT_CAN_BE_DAMAGED = 7, // Vehicle is locked but can still be damaged.
+-- VEHICLELOCK_LOCKED_BUT_BOOT_UNLOCKED = 8, // Vehicle is locked, but its trunk/boot remains unlocked.
+-- VEHICLELOCK_LOCKED_NO_PASSENGERS = 9, // Vehicle is locked and does not allow passengers, except for the driver.
+-- VEHICLELOCK_CANNOT_ENTER = 10 // Vehicle is completely locked, preventing entry entirely, even if previously inside.
 -- };
+-- ```
+-- @param vehicle The vehicle whose doors are to be locked.
+-- @param doorLockStatus The lock state to apply to the vehicle's doors, see `eVehicleLockState`.
 function Global.SetVehicleDoorsLocked(vehicle, doorLockStatus)
 	return _in(0x4cdd35d0, vehicle, doorLockStatus)
 end
